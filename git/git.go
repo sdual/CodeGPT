233ef4cb2b233ae2eaff61e8a5b48c4e6b0ba15e
Norihito Shirai <shr.nrht.0623@gmail.com>
2025-10-27 18:59:26 +0900

    customize commit messages

--- git/git.go
+++ git/git.go
@@ -121,8 +121,6 @@ func (c *Command) gitDir() *exec.Cmd {
 func (c *Command) commit(val string) *exec.Cmd {
 	args := []string{
 		"commit",
-		"--no-verify",
-		"--signoff",
 		fmt.Sprintf("--message=%s", val),
 	}
 
